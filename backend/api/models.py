f0a485665180278b7fe16e31f329e826244769ce
Matt Wiens <mwiens91@gmail.com>
2020-01-11 15:36:02 -0800

    Add basic models to backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,5 @@
-# TODO: probably want these later
-#from django.conf import settings
-#from django.db import models
-
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -11,11 +8,42 @@ from rest_framework.authtoken.models import Token
 class User(AbstractUser):
     """Custom user model."""
 
+    track_nicotine = models.BooleanField(
+        default=True, help_text="Whether to track nicotine consumption."
+    )
+    track_alcohol = models.BooleanField(
+        default=True, help_text="Whether to track alcohol consumption."
+    )
+
     class Meta:
         """Model metadata."""
 
         # Order by username in ascending order
         ordering = ["username"]
+
+
+class AbstractDataPoint(models.Model):
+    """Sets up basic structure for data points.
+
+    This is an abstract model, so it doesn't create a table in the database.
+    """
+
+    datetime = models.DateTimeField(auto_now_add=True)
+    quantity = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class DataPointNicotine(AbstractDataPoint):
+    # TODO: add a category field here with list of choices
+    pass
+
+
+class DataPointAlcohol(AbstractDataPoint):
+    # TODO: add a category field here with list of choices
+    pass
 
 
 @receiver(post_save, sender=User)
