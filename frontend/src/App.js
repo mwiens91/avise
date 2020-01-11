8e3704b559a2696bd9cd6e9b5a68eac41203488a
Tyler Trinh <tylervtrinh@gmail.com>
2020-01-11 13:57:13 -0800

    Add react snippet and navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavBar from './components/AppNavBar';
+
+class App extends Component {
+	render() {
+		return <AppNavBar />;
+	}
+}
+
+export default App;
