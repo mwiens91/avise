c80fd0752ebba40bfe4854bdb1bd2634428c1ba1
Cloud7831 <JaydenMcPhedrain@gmail.com>
2020-01-11 18:03:12 -0800

    Added daily limits for beer/whatever other product

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-let beerCount = 0;
-// Configure logger settings
 
+//Note, in the future, beerCount will be tracked by the server so that each user has their own data.
+
+let beerCount = 0; 
+let dailyBeerLimit = 4;
+let weeklyBeerLimit = 8;
+// Configure logger settings
+console.log("Starting Bot");
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
@@ -15,6 +20,7 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+console.log("Bot initialized");
 bot.on('ready', function (evt) {
     console.log('Discord Bot Connected');
     console.log(`Logged in as: ${bot.username}`);
@@ -36,19 +42,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
            	case 'beer':
-
-           		beerCount++;
-           		let msgString = "";
-
-           		//Set amount of beers drank
-           		msgString += `${user} you've drank ${beerCount} `;
-           		if(beerCount == 1){
-           			msgString += "beer!";
-           		}
-           		else{
-           			msgString += "beers!";
-           		}
            		
+           		beerCount++;
+           		
+           		let msgString = loggerStringBuilder("beer", user);
            		bot.sendMessage({
                     to: channelID,
                     message: msgString
@@ -58,3 +55,58 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
+
+
+
+function loggerStringBuilder(product, user){
+	let msgString = "";
+	let dailyLimit;
+	let weeklyLimit;
+	let count;
+	let productSing;
+	let productPlur;
+
+	if(product == "beer"){
+		dailyLimit = dailyBeerLimit;
+		weeklyLimit = weeklyBeerLimit;
+		count = beerCount;
+		productPlur = "beers";
+		productSing = "beer";
+	}
+	else if(product == "nicotine"){
+		// TODO: 
+	}
+
+    msgString += `${user} you've `;
+   	if(product == "beer"){
+   		msgString += "drank ";
+   	}
+
+    msgString += `${count} `;
+	if(count == 1){
+		msgString += `${productSing}.`;
+	}
+	else{
+		msgString += `${productPlur}.`;
+	}
+
+	if(dailyLimit - count == 0){
+		msgString += "You have reached your daily drinking limit and should not continue drinking!";
+	}
+	else if(dailyLimit - count < 0){
+		msgString += "You have surpassed your daily drinking limit! Your liver is crying and would like you to please stop. ";
+	}
+	else if(dailyLimit - count <= 2){
+		msgString += `You are nearing your desired daily beer limit of ${dailyLimit} `;
+		if(dailyLimit == 1){
+			msgString += "beer.";
+		}
+		else{
+			msgString += "beers.";
+		}
+	}
+
+
+
+	return msgString;
+}
