58b29ba14fa1f366e817c2d0b9ba9407fa9364a3
Matt Wiens <mwiens91@gmail.com>
2020-01-11 21:11:36 -0800

    Add dotenv stuff to WSGI in backend

--- backend/nwhacks2020/wsgi.py
+++ backend/nwhacks2020/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import dotenv
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nwhacks2020.settings")
 
